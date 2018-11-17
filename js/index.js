7b505a1565d03b9cd9533e5cd702939fdf9ae974
Pedro <pedrobrito17@gmail.com>
2018-11-17 17:28:51 -0300

    portfolio finalizado

--- js/index.js
+++ js/index.js
@@ -8,8 +8,43 @@ $('li a').click(function(){
 });
 
 
-$(".abrirModal").click(function() {
-    var url = $(this).find("img").attr("src");
-    $("#myModal img").attr("src", url);
-    $("#myModal").modal("show");
+
+
+
+
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementsByClassName('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+var navbar;
+
+$('.myImg').click(function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+
+    navbar = $('#navbar').is(':visible');
+    if(navbar){
+        $('#navbar').hide();
+    }else{
+        $('#nav-pills').hide();
+    }
 });
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+
+  if(navbar){
+    $('#navbar').show();
+  }else{
+    $('#nav-pills').show();
+  }
+}
